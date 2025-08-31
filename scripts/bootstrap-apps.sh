21b70fbf06eaf67643e5160937d3a657728fe964
1activegeek <shawnmix@gmail.com>
2025-08-30 21:36:47 -0400

    Add snapshot-controller

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -93,6 +93,8 @@ function apply_crds() {
         https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.85.0/stripped-down-crds.yaml
         # renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
         https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
+        # renovate: datasource=github-releases depName=kubernetes-csi/external-snapshotter
+        https://github.com/kubernetes-csi/external-snapshotter/releases/download/v7.0.2/crd-list.yaml
         # renovate: datasource=github-releases depName=kubernetes-sigs/external-dns
         https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.18.0/docs/sources/crd/crd-manifest.yaml
     )
